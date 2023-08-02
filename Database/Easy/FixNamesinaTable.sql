5cc282b9126689dd6a8bb3071f923e896e7bd33c
Parviz Yuldashev <www.parviz98@ya.ru>
2023-08-02 21:34:45 +0500

    Add: 1683. Invalid Tweets

--- Database/Easy/FixNamesinaTable.sql
+++ Database/Easy/FixNamesinaTable.sql
@@ -1,2 +1,9 @@
 SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(RIGHT(name, LENGTH(name)-1))) AS name FROM users
 ORDER BY user_id;
+
+/* Write your T-SQL query statement below */
+SELECT
+  [user_id], 
+  CONCAT(UPPER(SUBSTRING([name], 1, 1)), LOWER(SUBSTRING([name], 2, LEN([name])))) AS 'name'
+FROM users
+ORDER BY [user_id]
