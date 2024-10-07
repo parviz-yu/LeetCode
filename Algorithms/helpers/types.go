d57f4225a9faab8e341ed95637341cc1a782397c
Parviz Yuldoshev <www.parviz98@ya.ru>
2024-10-07 13:31:47 +0500

    Add: 225. Implement Stack using Queues

--- Algorithms/helpers/types.go
+++ Algorithms/helpers/types.go
@@ -4,3 +4,33 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+type Queue struct {
+	Head *ListNode
+	Tail *ListNode
+	Size int
+}
+
+func (q *Queue) Enqueue(x int) {
+	node := &ListNode{Val: x}
+	q.Size++
+	if q.Tail == nil {
+		q.Tail = node
+		q.Head = node
+		return
+	}
+
+	q.Tail.Next = node
+	q.Tail = node
+}
+
+func (q *Queue) Dequeue() int {
+	node := q.Head
+	q.Head = node.Next
+	q.Size--
+	if q.Size == 0 {
+		q.Tail = nil
+	}
+
+	return node.Val
+}
